c9decb974dc67b60b9d69f3b5cafb97e43a6ac6e
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-17 18:27:04 +0530

    Update Main.java

--- Day1/Session2/Main.java
+++ Day1/Session2/Main.java
@@ -1,1 +1,8 @@
+package com.designpatterns.handson.day1.session2.part1.dbclass;
 
+public class Main {
+	public static void main(String[] args) {
+		DBConn instance =  DBConn.getInstance();
+		DBConn.getHashCode();
+	}
+}
