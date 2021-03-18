d64a9c5b25c1820d5927737496edfb24a964b900
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-18 13:46:25 +0530

    Update Main.java

--- Day2/Practice/BuilderDesignPattern/Main.java
+++ Day2/Practice/BuilderDesignPattern/Main.java
@@ -1,1 +1,15 @@
+public class Main {
 
+	public static void main(String[] args) {
+		HouseBuilder iglooBuilder = new IglooHouseBuilder();
+		
+		CivilEngineer engineer = new CivilEngineer(iglooBuilder);
+		
+		engineer.construcHouse();
+		
+		House house = engineer.getHouse();
+		
+		System.out.println("Builder constructed: " + house);
+	}
+
+}
