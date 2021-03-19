336101282b9309b078a94b7b1430643f9ddbb6ae
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-19 12:11:15 +0530

    Add files via upload

---
+++ Day2/FacadePattern/Circle.java
@@ -0,0 +1,9 @@
+//Third Concrete Class, implementing the Shape Interface
+public class Circle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Circle::draw()");
+	}
+
+}
---
+++ Day2/FacadePattern/Rectangle.java
@@ -0,0 +1,8 @@
+//First concrete class, implementing the Shape Interface
+public class Rectangle implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Rectangle::draw()");
+	}
+
+}
