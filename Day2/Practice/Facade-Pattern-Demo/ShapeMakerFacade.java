4277ce7b3f01f2cb4e48c4daf17eaa2c2890f6a6
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-19 11:54:08 +0530

    Add files via upload

---
+++ Day2/Practice/Facade-Pattern-Demo/ShapeMakerFacade.java
@@ -0,0 +1,24 @@
+//The Facade Class
+public class ShapeMakerFacade {
+	private Shape circle;
+	private Shape rectangle;
+	private Shape square;
+	
+	public ShapeMakerFacade() {
+		circle = new Circle();
+		rectangle = new Rectangle();
+		square = new Square();
+	}
+	
+	public void drawCircle() {
+		circle.draw();
+	}
+	
+	public void drawRectangle() {
+		rectangle.draw();
+	}
+	
+	public void drawSquare() {
+		square.draw();
+	}
+}
---
+++ Day2/Practice/Facade-Pattern-Demo/Square.java
@@ -0,0 +1,7 @@
+//Second Concrete Class, implementing the Shape Interface
+public class Square implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Square::draw()");
+	}
+}
