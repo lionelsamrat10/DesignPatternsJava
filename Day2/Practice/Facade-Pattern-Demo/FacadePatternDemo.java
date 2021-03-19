9b334c41e0e85c7d8f053cae34b206e3313f2f1e
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-19 11:52:42 +0530

    Create FacadePatternDemo.java

---
+++ Day2/Practice/Facade-Pattern-Demo/FacadePatternDemo.java
@@ -0,0 +1,18 @@
+//The client uses only the ShapeMakerFacade Class
+public class FacadePatternDemo {
+	public static void main(String[] args) {
+		ShapeMakerFacade smf = new ShapeMakerFacade();
+		
+		//Call the draw methods
+		smf.drawCircle();
+		smf.drawRectangle();
+		smf.drawSquare();
+	}
+}
+
+//Expected Output
+/*
+Circle::draw()
+Rectangle::draw()
+Square::draw()
+*/
